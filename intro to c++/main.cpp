10aedac98fecb9d6b79330ea0478fa77c05aaec7
Holiday Course <holiday@ad.aie.edu>
2018-08-13 16:28:37 -0500

    Add project files.

---
+++ intro to c++/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Robert" << std:: endl;
+	std::cout << "apple" << std::endl;
+	std::cout << "pinapple" << std::endl;
+	std::cout << "1. Rap God" << std::endl;
+	std::cout << "2. My Songs Know What you did in the Dark" << std::endl;
+	std::cout << "3. Ms. Jackson" << std::endl;
+	std::cout << "4. immortals" << std::endl;
+	std::cout << "5. War of Change" << std::endl;
+	std::cout << "6. gasoline" << std::endl;
+	std::cout << "7. get me out of here" << std::endl;
+	std::cout << "8. Go!!!" << std::endl;
+	std::cout << "9. wake me up" << std::endl;
+	std::cout << "10. I" << std::endl;
+
+	std::cout << "0" << std::endl;
+	std::cout << "1" << std::endl;
+	std::cout << "2" << std::endl;
+	std::cout << "3" << std::endl;
+	std::cout << "4" << std::endl;
+	std::cout << "5" << std::endl;
+
+	std::cout << "5" << std::endl;
+	std::cout << "4" << std::endl;
+	std::cout << "3" << std::endl;
+	std::cout << "2" << std::endl;
+	std::cout << "1" << std::endl;
+	std::cout << "0" << std::endl;
+
+	system("pause");
+}
